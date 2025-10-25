0692eb8556e9cbcb1b18493bda775e4ae2f30d53
Amandeep Prajapati <amandeep.p.2015@gmail.com>
2025-10-25 23:18:19 +0530

    chat app

---
+++ backend/user/dist/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const connectDb = async () => {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+        throw new Error('MongoDB URI not found in env variable');
+    }
+    try {
+        await mongoose.connect(uri, {
+            dbName: "ChapAppMicroservice"
+        });
+        console.log('MongoDB connected');
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+export default connectDb;
+//# sourceMappingURL=db.js.map
