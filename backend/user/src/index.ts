0963567122838f1e7b43644f2bd5ff6185b71e2f
Amandeep Prajapati <amandeep.p.2015@gmail.com>
2025-10-25 23:35:29 +0530

    setup complete

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js';
+import { createClient } from 'redis';
 
 dotenv.config();
 connectDb()
+
+export const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+})
+
+redisClient.connect()
+    .then(() => {
+        console.log('Connected to Redis');
+    }).catch((err) => {
+        console.log('Error connecting to Redis', err);
+    })
 
 const app = express();
 
